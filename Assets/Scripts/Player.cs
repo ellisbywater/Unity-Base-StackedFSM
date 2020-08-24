f64a598b7e59c0b0bcb4df96bf4c733823c861cf
Ellis G <egaraudy@gmail.com>
2020-08-24 14:24:34 -0600

    create player health script and update animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,13 +8,16 @@ public class Player : MonoBehaviour
     private Animator _animator;
     private float speed;
     private Rigidbody _rigidbody;
+    private Health _health;
     
     // Start is called before the first frame update
     void Start()
     {
+        _health = GetComponent<Health>();
         _animator = transform.GetChild(0).GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody>();
         speed = 1.5f;
+        _health.SetHealth(20);
     }
 
     // Update is called once per frame
@@ -26,11 +29,20 @@ public class Player : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementVector), 0.25f );
         }
         _animator.SetBool("Walking", movementVector != Vector3.zero);
+        if (Input.GetKeyDown((KeyCode.I)))
+        {
+            Hurt(2, 0.2f);
+        }
     }
 
     void CalculateMovement()
     {
         movementVector = new Vector3(Input.GetAxis("Horizontal"), _rigidbody.velocity.y, Input.GetAxis("Vertical"));
         _rigidbody.velocity = new Vector3(movementVector.x * speed, movementVector.y, movementVector.z * speed);
+    }
+
+    public void Hurt(int amount, float delay = 0.5f)
+    {
+        StartCoroutine(_health.TakeDamageDelayed(amount, delay));
     }
 }
